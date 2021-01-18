60744995443435a456246f21475f8e16e5b92fbf
Hernan Forni <hernan_forni@antina.com.ar>
2021-01-18 09:24:19 -0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApplicationPruebaPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.buttonBorrar = new System.Windows.Forms.Button();
+            this.sNumeroComprobante = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightCyan;
+            this.button1.Location = new System.Drawing.Point(35, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generar PDF";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightPink;
+            this.button2.Location = new System.Drawing.Point(246, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 332);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1053, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.statusStrip1_ItemClicked);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(300, 16);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PeachPuff;
+            this.button3.Location = new System.Drawing.Point(37, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 51);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Temp Folder";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // buttonBorrar
+            // 
+            this.buttonBorrar.Location = new System.Drawing.Point(246, 127);
+            this.buttonBorrar.Name = "buttonBorrar";
+            this.buttonBorrar.Size = new System.Drawing.Size(184, 51);
+            this.buttonBorrar.TabIndex = 5;
+            this.buttonBorrar.Text = "Borrar";
+            this.buttonBorrar.UseVisualStyleBackColor = true;
+            this.buttonBorrar.Click += new System.EventHandler(this.buttonBorrar_Click);
+            // 
+            // sNumeroComprobante
+            // 
+            this.sNumeroComprobante.Location = new System.Drawing.Point(35, 12);
+            this.sNumeroComprobante.Name = "sNumeroComprobante";
+            this.sNumeroComprobante.Size = new System.Drawing.Size(395, 96);
+            this.sNumeroComprobante.TabIndex = 6;
+            this.sNumeroComprobante.Text = "1B*2100000082";
+            this.sNumeroComprobante.TextChanged += new System.EventHandler(this.sNumeroComprobante_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(636, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(386, 238);
+            this.listBox1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightGreen;
+            this.button4.Location = new System.Drawing.Point(37, 241);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(187, 51);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Procesar Lote";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button6.Location = new System.Drawing.Point(246, 242);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(184, 50);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Web Service";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button5.Location = new System.Drawing.Point(436, 242);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(184, 50);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Web Service 85";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 354);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.sNumeroComprobante);
+            this.Controls.Add(this.buttonBorrar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "-";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonBorrar;
+        private System.Windows.Forms.RichTextBox sNumeroComprobante;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsFormsApplicationPruebaPDF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Drawing.Text;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+    using PdfSharp.Drawing;
+
+    public partial class Form1 : Form
+    {
+        //   XBrush BrushBlack = XBrushes.Black;
+        //  XFont fontArial5 = new XFont("Planet Benson 2", 5, XFontStyle.Bold, new XPdfFontOptions(PdfFontEmbedding.Always));
+
+        private static string GetFontNameFromFile(string filename)
+        {
+            PrivateFontCollection fontCollection = new PrivateFontCollection();
+            fontCollection.AddFontFile("c:\\fonts\\ufonts.com_gotham-bold.ttf");
+            return fontCollection.Families[0].Name;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            this.toolStripStatusLabel1.Text = string.Empty;
+            TelnetPDF.ClassParametrosTelnetPDF cp = new TelnetPDF.ClassParametrosTelnetPDF();
+            cp.FileRoute = "c:\\temp\\";         //"D:\\TelnetPDF\\WebApplicationTelnetPDF\\WebApplicationTelnetPDF\\files\\";
+            cp.Imageroute = ConfigurationSettings.AppSettings["ImageRoute"];
+            cp.Urlpath =ConfigurationSettings.AppSettings["Urlpath"].ToString(); // "/files/";
+            cp.Urlmensaje = ConfigurationSettings.AppSettings["Urlmensaje"].ToString(); // "Descargar Archivo Aqui";
+            cp.CantidadCamposxRegistro = ConfigurationSettings.AppSettings["CantidadCamposxRegistro"].ToString();
+            cp.CnnStringSqlServerSinPassword = ConfigurationSettings.AppSettings["CnnStringSqlServerSinPassword"].ToString();
+            cp.CuentaUniverse = ConfigurationSettings.AppSettings["CuentaUniverse"].ToString();    
+            cp.PasswordSqlServerEncriptado = ConfigurationSettings.AppSettings["PasswordSqlServerEncriptado"].ToString();
+            cp.PasswordUniverseEncriptado = ConfigurationSettings.AppSettings["PasswordUniverseEncriptado"].ToString();
+            cp.ServerUniverse = ConfigurationSettings.AppSettings["ServerUniverse"].ToString();
+            cp.UsuarioUniverse = ConfigurationSettings.AppSettings["UsuarioUniverse"].ToString();
+            cp.LogFile = ConfigurationSettings.AppSettings["PathLog"].ToString();
+            cp.FontRoute = ConfigurationSettings.AppSettings["FontRoute"].ToString();
+
+            TelnetPDF.comprobantesPDF ts = new TelnetPDF.comprobantesPDF();
+            string res=ts.GenerarFactura(this.sNumeroComprobante.Text.Trim(), cp, true, true);
+            toolStripStatusLabel1.Text = "Generacion Finalizada:" + res;
+           // MessageBox.Show("Generacion Finalizada:" + res, "OK", MessageBoxButtons.OK);
+
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"c:\temp\");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool ClearFolder(string folderName)
+        {
+
+            bool retvalue = true;
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(folderName);
+
+                foreach (FileInfo fi in dir.GetFiles())
+                {
+                    fi.Delete();
+                }
+
+                foreach (DirectoryInfo di in dir.GetDirectories())
+                {
+                    this.ClearFolder(di.FullName);
+                    di.Delete();
+                }
+            }
+            catch 
+            {
+                retvalue = false;
+                this.toolStripStatusLabel1.Text = "Hay Pdfs Abiertos";
+            }
+            return retvalue;
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            if (this.ClearFolder(@"c:\temp\")) { }
+                this.toolStripStatusLabel1.Text = "Borrado Finalizado";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+       
+
+
+        }
+
+
+ 
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string a = GetFontNameFromFile("");
+        }
+
+      
+
+        private List<string> ParseaCampo(string Parrafo, XFont font, double dRango, XGraphics graph)
+        {
+            List<string> retvalue = new List<string>();
+
+            var size = graph.MeasureString(Parrafo, font);
+            int a=1;
+            int rango = 100;
+          
+            if (size.Width > dRango)
+            {
+                int contador = 1;
+                int lastspace = 0;
+                double auxSize;
+
+                while (a < rango)
+                {
+                    if (Parrafo.Substring(contador, 1) == " ")
+                        lastspace = contador;
+
+                    var size1 = graph.MeasureString(Parrafo.Substring(0,contador), font);
+                    auxSize = size1.Width;
+                    contador++;
+                    a=(int)(auxSize);
+                }
+                retvalue.Add(Parrafo.Substring(0, lastspace));
+
+                a = 1;
+                contador = 1;
+                string subparrafo = Parrafo.Substring(lastspace).TrimStart();
+                int maxlenght = subparrafo.Length;
+                lastspace = 0;
+                while ((a < rango)&&(contador<maxlenght))
+                {
+                    var size1 = graph.MeasureString(subparrafo.Substring(0, contador), font);
+                    auxSize = size1.Width;
+                    contador++;
+                    a = (int)(auxSize);
+                }
+              
+                retvalue.Add(subparrafo.Substring(0, contador));
+
+            }
+            else
+            {
+                retvalue.Add(Parrafo);
+            }
+            return retvalue;
+
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+
+            Cursor.Current = Cursors.WaitCursor;
+            toolStripStatusLabel1.Text = "";
+
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            int counter = 0;  
+            string line;
+            IList<string> DatosComprobante = new List<string>();
+
+
+            Cursor.Current = Cursors.WaitCursor;
+            toolStripStatusLabel1.Text = "";
+
+            TelnetPDF.ClassParametrosTelnetPDF cp = new TelnetPDF.ClassParametrosTelnetPDF
+            {
+                FileRoute = "c:\\temp\\",         //"D:\\TelnetPDF\\WebApplicationTelnetPDF\\WebApplicationTelnetPDF\\files\\";
+
+                Imageroute = ConfigurationSettings.AppSettings["ImageRoute"],
+                Urlpath = ConfigurationSettings.AppSettings["Urlpath"].ToString(), // "/files/";
+                Urlmensaje = ConfigurationSettings.AppSettings["Urlmensaje"].ToString(), // "Descargar Archivo Aqui";
+                Archivo = ConfigurationSettings.AppSettings["Archivo"].ToString(),
+                Archivo2 = ConfigurationSettings.AppSettings["Archivo2"].ToString(),
+                CantidadCamposxRegistro = ConfigurationSettings.AppSettings["CantidadCamposxRegistro"].ToString(),
+                CnnStringSqlServerSinPassword = ConfigurationSettings.AppSettings["CnnStringSqlServerSinPassword"].ToString(),
+                CuentaUniverse = ConfigurationSettings.AppSettings["CuentaUniverse"].ToString(),
+
+                PasswordSqlServerEncriptado = ConfigurationSettings.AppSettings["PasswordSqlServerEncriptado"].ToString(),
+                PasswordUniverseEncriptado = ConfigurationSettings.AppSettings["PasswordUniverseEncriptado"].ToString(),
+                Path = ConfigurationSettings.AppSettings["Path"].ToString(),
+                Path2 = ConfigurationSettings.AppSettings["Path2"].ToString(),
+                ServerUniverse = ConfigurationSettings.AppSettings["ServerUniverse"].ToString(),
+                UsuarioUniverse = ConfigurationSettings.AppSettings["UsuarioUniverse"].ToString(),
+                LogFile = ConfigurationSettings.AppSettings["PathLog"].ToString(),
+                FontRoute = ConfigurationSettings.AppSettings["FontRoute"].ToString()
+            };
+
+            TelnetPDF.comprobantesPDF ts = new TelnetPDF.comprobantesPDF();
+
+
+             
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    System.IO.StreamReader file = new System.IO.StreamReader(@openFileDialog1.FileName);
+                    int iSkip = 0;
+                       toolStripProgressBar1.Value = 0;
+                        while ((line = file.ReadLine()) != null) 
+                          {
+                              line = line.Replace('þ', '|');
+                            DatosComprobante.Add(line);
+                            counter++;
+                         }
+                       
+
+                        toolStripProgressBar1.Maximum = counter;
+                        int LngLotes;
+                        if (counter < 250)
+                            LngLotes = counter;
+                        else
+                            LngLotes = 250;
+                        
+                       //analizo la cantidad de lotes a procesar
+                        int cantidadlotes = counter / LngLotes;
+
+
+                        for (int i = 1; i <= cantidadlotes; i++)
+                        {
+                            var subList = DatosComprobante.Skip(iSkip).Take(LngLotes).ToList();
+                          //  if (i == 4)
+                          //  {
+                                string p = ts.GenerarFacturasLotes(subList, cp, true, true, i.ToString());
+                           // }
+                                //list.Skip(2).Take(5).ToList();
+                            toolStripProgressBar1.Value = (i * LngLotes);
+                            iSkip = iSkip + LngLotes;
+                        }
+
+                        Cursor.Current = Cursors.Default;
+                        toolStripStatusLabel1.Text = "Lote Creado";
+
+                }
+
+                  
+               
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+       
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            WsGeneracionComprobantes.pdfgen.prod.WSGeneracionComprobantes ws = new WsGeneracionComprobantes.pdfgen.prod.WSGeneracionComprobantes();
+           string VariablePerturbadora= ws.SGenerarComprobanteruta(sNumeroComprobante.Text.Trim(), @"c:\inetpub\wwwroot\newmailer\files\");
+            var url = "http://facturas.antina.com.ar/files/" + VariablePerturbadora;
+            toolStripStatusLabel1.Text = VariablePerturbadora;
+ 
+        }
+
+        private void sNumeroComprobante_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            wsgeneracioncomprobantes85.WSGeneracionComprobantes ws = new wsgeneracioncomprobantes85.WSGeneracionComprobantes();
+            string VariablePerturbadora = ws.SGenerarComprobanteruta(this.sNumeroComprobante.Text.Trim(), @"C:\inetpub\wwwroot\DescargaComprobante\files\");
+            var url = "http://facturas.antina.com.ar/files/" + VariablePerturbadora;
+            toolStripStatusLabel1.Text = VariablePerturbadora;
+        }
+    }
+}
